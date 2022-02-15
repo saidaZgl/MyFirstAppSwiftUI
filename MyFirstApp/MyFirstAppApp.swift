f4bf864f6d7f28561ec96974a94f9d9b735665ee
Saida Zghal <zghal.saida@orange.fr>
2022-02-15 23:11:40 +0100

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Saida Zghal on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
